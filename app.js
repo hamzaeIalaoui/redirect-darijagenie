9be3be1e204610e3e411018135a65cffae05c882
Hamza El Alaoui <62178890+hamzaeIalaoui@users.noreply.github.com>
2023-04-20 02:08:51 +0000

    initial com

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(302, {
+    'Location': 'http://20.74.117.191:3000/',
+  });
+  res.end();
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`Redirection app listening on port ${port}`));
